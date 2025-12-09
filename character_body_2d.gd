6d3a44c47ad7c40cb0fff3cd18bb968faea6852f
Chickenhatter <234830516+Chickenhatter@users.noreply.github.com>
2025-12-09 21:58:29 +1300

    Initial commit

---
+++ character_body_2d.gd
@@ -0,0 +1,23 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta: float) -> void:
+	var input_vector = Vector2.ZERO
+	if Input.is_action_pressed("ui_w"):
+		input_vector.y -= 1
+	if Input.is_action_pressed("ui_s"):
+		input_vector.y += 1
+	if Input.is_action_pressed("ui_a"):
+		input_vector.x += 1
+	if Input.is_action_pressed("ui_d"):
+		input_vector.x -= 1
+	velocity = input_vector * 300
+	move_and_slide()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	get_tree().change_scene_to_file("")
